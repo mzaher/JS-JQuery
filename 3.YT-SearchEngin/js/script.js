2f9f65b5300fccabc160852bc773b67312e0c633
mohcoder <moh.zaher.coder@gmail.com>
2015-12-17 14:44:47 +0300

    commiting fancybox files & get buttons work

--- 3.YT-SearchEngin/js/script.js
+++ 3.YT-SearchEngin/js/script.js
@@ -1,74 +1,164 @@
-//Search_bar handler
+// Searchbar Handler
 $(function () {
-    var searchField = $("#query");
-    var icon = $("#search-btn");
+    var searchField = $('#query');
+    var icon = $('#search-btn');
 
-    //focus event handler
-    $(searchField).on("focus", function () {
+    // Focus Event Handler
+    $(searchField).on('focus', function () {
         $(this).animate({
-            width: "100%"
+            width: '100%'
         }, 400);
         $(icon).animate({
-            right: "10px"
+            right: '10px'
         }, 400);
     });
 
-    //Blur Event Handler
-    $(searchField).on("blur", function () {
-        if (searchField.val() == "") {
+    // Blur Event Handler
+    $(searchField).on('blur', function () {
+        if (searchField.val() == '') {
             $(searchField).animate({
-                width: "45%"
+                width: '45%'
             }, 400, function () {});
             $(icon).animate({
-                right: "360px"
+                right: '360px'
             }, 400, function () {});
-        };
+        }
     });
 
     $('#search-form').submit(function (e) {
         e.preventDefault();
     });
-});
+})
 
 //Search Function
 function search() {
-    //Clear Results
-    $("#results").html("");
-    $("buttons").html("");
+    // Clear Results
+    $('#results').html('');
+    $('#buttons').html('');
 
-    //Get form input
-    q = $("#query").val();
+    // Get Form Input
+    q = $('#query').val();
 
-    //Run get request on API
+    // Run GET Request on API
     $.get(
         "https://www.googleapis.com/youtube/v3/search", {
-            part: "snippet, id",
+            part: 'snippet, id',
             q: q,
-            type: "video",
-            maxResults: 10,
-            key: "AIzaSyAXDUVnD7XMslWKOFB1aZ594z34jbtt46c"
+            type: 'video',
+            key: 'AIzaSyAXDUVnD7XMslWKOFB1aZ594z34jbtt46c'
         },
         function (data) {
             var nextPageToken = data.nextPageToken;
             var prevPageToken = data.prevPageToken;
+
+            // Log Data
             console.log(data);
 
             $.each(data.items, function (i, item) {
-                //Get Output
+                // Get Output
                 var output = getOutput(item);
 
-                //Display results
-                $("#results").append(output);
+                // Display Results
+                $('#results').append(output);
             });
+
             var buttons = getButtons(prevPageToken, nextPageToken);
 
-            //Display buttons
-            $("#buttons").append(buttons);
+            // Display Buttons
+            $('#buttons').append(buttons);
         }
     );
 }
 
-//Build output
+// Next Page Function
+function nextPage() {
+    var token = $('#next-button').data('token');
+    var q = $('#next-button').data('query');
+
+    // Clear Results
+    $('#results').html('');
+    $('#buttons').html('');
+
+    // Get Form Input
+    q = $('#query').val();
+
+    // Run GET Request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: q,
+            pageToken: token,
+            type: 'video',
+            key: 'AIzaSyAXDUVnD7XMslWKOFB1aZ594z34jbtt46c'
+        },
+        function (data) {
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+
+            // Log Data
+            console.log(data);
+
+            $.each(data.items, function (i, item) {
+                // Get Output
+                var output = getOutput(item);
+
+                // Display Results
+                $('#results').append(output);
+            });
+
+            var buttons = getButtons(prevPageToken, nextPageToken);
+
+            // Display Buttons
+            $('#buttons').append(buttons);
+        }
+    );
+}
+
+// Prev Page Function
+function prevPage() {
+    var token = $('#prev-button').data('token');
+    var q = $('#prev-button').data('query');
+
+    // Clear Results
+    $('#results').html('');
+    $('#buttons').html('');
+
+    // Get Form Input
+    q = $('#query').val();
+
+    // Run GET Request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: q,
+            pageToken: token,
+            type: 'video',
+            key: 'AIzaSyAXDUVnD7XMslWKOFB1aZ594z34jbtt46c'
+        },
+        function (data) {
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+
+            // Log Data
+            console.log(data);
+
+            $.each(data.items, function (i, item) {
+                // Get Output
+                var output = getOutput(item);
+
+                // Display Results
+                $('#results').append(output);
+            });
+
+            var buttons = getButtons(prevPageToken, nextPageToken);
+
+            // Display Buttons
+            $('#buttons').append(buttons);
+        }
+    );
+}
+
+// Build Output
 function getOutput(item) {
     var videoId = item.id.videoId;
     var title = item.snippet.title;
@@ -77,30 +167,35 @@ function getOutput(item) {
     var channelTitle = item.snippet.channelTitle;
     var videoDate = item.snippet.publishedAt;
 
-    //Build output string
+    // Build Output String
     var output = '<li>' +
         '<div class="list-left">' +
         '<img src="' + thumb + '">' +
         '</div>' +
         '<div class="list-right">' +
-        '<h3>' + title + '</h3>' +
+        '<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/' + videoId + '">' + title + '</a></h3>' +
         '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
         '<p>' + description + '</p>' +
         '</div>' +
         '</li>' +
-        '<div class="clearfix"></div>'
-    '';
+        '<div class="clearfix"></div>' +
+        '';
+
     return output;
 }
 
-// Build Buttons (prev, next)
+// Build the buttons
 function getButtons(prevPageToken, nextPageToken) {
     if (!prevPageToken) {
-        var btnoutput = '<div class="button-container">' + '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '" onclick="nextPage();">Next Page</button></div>';
-
+        var btnoutput = '<div class="button-container">' + '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            'onclick="nextPage();">Next Page</button></div>';
     } else {
-        var btnoutput = '<div class="button-container"><button id="next-button" class="paging-button" data-token="' + prevPageToken + '" data-query="' + q + '" onclick="prevPage();">Next Page</button><button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '" onclick="nextPage();">Next Page</button></div>';
-
+        var btnoutput = '<div class="button-container">' +
+            '<button id="prev-button" class="paging-button" data-token="' + prevPageToken + '" data-query="' + q + '"' +
+            'onclick="prevPage();">Prev Page</button>' +
+            '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            'onclick="nextPage();">Next Page</button></div>';
     }
+
     return btnoutput;
 }
