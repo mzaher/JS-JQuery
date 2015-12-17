b7596bfca1cdf0f1b772629a9722146d6fb249d0
mohcoder <moh.zaher.coder@gmail.com>
2015-12-17 13:25:25 +0300

    commiting  YT API javascript

--- 3.YT-SearchEngin/js/script.js
+++ 3.YT-SearchEngin/js/script.js
@@ -1,4 +1,4 @@
-//Searchbar handler
+//Search_bar handler
 $(function () {
     var searchField = $("#query");
     var icon = $("#search-btn");
@@ -29,3 +29,62 @@ $(function () {
         e.preventDefault();
     });
 });
+
+//Search Function
+function search() {
+    //Clear Results
+    $("#results").html("");
+    $("buttons").html("");
+
+    //Get form input
+    var q = $("#query").val();
+
+    //Run get request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: "snippet, id",
+            q: q,
+            type: "video",
+            maxResults: 10,
+            key: "AIzaSyAXDUVnD7XMslWKOFB1aZ594z34jbtt46c"
+        },
+        function (data) {
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+            console.log(data);
+
+            $.each(data.items, function (i, item) {
+                //Get Output
+                var output = getOutput(item);
+
+                //Display results
+                $("#results").append(output);
+            });
+        }
+    );
+}
+
+//Build output
+function getOutput(item) {
+    var videoId = item.id.videoId;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var videoDate = item.snippet.puplishedAt;
+
+    //Build output string
+    var output = '<li>' +
+        '<div class="list-left">' +
+        '<img src="' + thumb + '">' +
+        '</div>' +
+        '<div class="list-right">' +
+        '<h3>' + title + '</h3>' +
+        '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
+        '<p>' + description + '</p>' +
+        '</div>' +
+        '</li>' +
+        '<div class="clearfix"></div>'
+    '';
+    return output;
+}
