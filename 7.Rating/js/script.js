930fb35c144fe7b822c7cd437418ad2237b6fac2
mohcoder <moh.zaher.coder@gmail.com>
2015-12-24 16:04:19 +0300

    committing js

--- 7.Rating/js/script.js
+++ 7.Rating/js/script.js
@@ -1,1 +1,14 @@
+$(function () {
+    $('.rating-circle').click(function () {
+        $(this).prevAll().andSelf().addClass('rating-chosen');
+        $(this).nextAll().removeClass('rating-chosen');
+    });
+    $('.rating-circle').hover(function () {
+        $(this).prevAll().andSelf().addClass('rating-hover');
+        $(this).nextAll().removeClass('rating-chosen');
+    });
+    $('.rating-circle').mouseleave(function () {
+        $(this).nextAll().removeClass('rating-hover');
+    })
 
+});
